76f77cafe87b63d588161f2d3c3f97f2b63c994f
RafaS92 <manrvaldez..92@gmail.com>
2020-12-07 17:59:54 -0600

    services complete

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,27 +1,25 @@
 import React from "react";
 import "./Services.css";
+import ServicesItem from "./ServicesItem";
 import * as servicesData from "../data/servicesData.json";
 
 function Services() {
   return (
     <>
       <div className="top-lay">
-        <img
-          src="https://www.kansashealthsystem.com/-/media/Project/Website/Hero/Care_Telehealth_AS304453347_hero.jpg?h=586&w=1440&la=en&hash=E97111E4AE5A56DDE1F710FE3E3BD60041C1FC58"
-          className="top-img"
-        />
+        <img className="top-img" />
       </div>
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
             <ServicesItem
-              src="https://images.pexels.com/photos/3872373/pexels-photo-3872373.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              src="https://www.med.unc.edu/pediatrics/files/2018/05/Peds_EM_Banner1-800x420.jpg"
               text="Eating plan with emphasizes vegetables, fruits, whole grains, and fat-free or low-fat dairy products."
               label="Alimentary Plan"
               path="/services"
             />
             <ServicesItem
-              src="https://images.pexels.com/photos/1112633/pexels-photo-1112633.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              src="https://hutchhealth.com/wp-content/uploads/2019/04/OccMed-01.jpg"
               text="Personalized workout plans your clients will love! Access the full video library with hundreds of pre-recorded exercises for you."
               label="Workout Rutines"
               path="/services"
@@ -29,19 +27,19 @@ function Services() {
           </ul>
           <ul className="cards__items">
             <ServicesItem
-              src="https://images.pexels.com/photos/4173251/pexels-photo-4173251.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+              src="https://cdn.shopify.com/s/files/1/2513/1396/files/face-place-medical-aesthetics-banner_grande.jpg?v=1525792385"
               text="Medical, surgical, dental, and professional dietitian."
               label="Medical Services"
               path="/services"
             />
             <ServicesItem
-              src="https://images.pexels.com/photos/4045549/pexels-photo-4045549.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+              src="https://www.marketplace.org/wp-content/uploads/2018/06/GettyImages-71265251.jpg?fit=1800%2C1000"
               text="Sleep therapies to prevent sleep disorders or insomnia."
               label="Sleep Therapy"
               path="/services"
             />
             <ServicesItem
-              src="https://images.pexels.com/photos/2294353/pexels-photo-2294353.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+              src="https://img.freepik.com/free-vector/mental-health-women-vector_53876-61377.jpg?size=338&ext=jpg&ga=GA1.2.1101768578.1606848787"
               text="Classes featured include beginner yoga classes, vinyasa yoga, hatha yoga, free meditation classes."
               label="Yoga Classes"
               path="/services"
@@ -49,7 +47,7 @@ function Services() {
           </ul>
           <ul className="cards__items">
             <ServicesItem
-              src="https://images.pexels.com/photos/3984353/pexels-photo-3984353.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              src="https://d2ngl0nkh8z0ib.cloudfront.net/dragonfly-uploads/2018/12/20/17/17/51/b0a6c2a7-23e5-4685-b7f3-bb07e1b2273c/Elderly_20woman_20laughing_20with_20her_20nurse-_20640_20x_20460.jpg"
               text="What makes us different?"
               text2="You, the client is what makes us different. Our job is to become a catalyst for your results. We are highly educated and knowledgeable, however you are the most important component in the equation."
               label="Why us?"
